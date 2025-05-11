0251c7c98458bbe242ff16a840c4ab5f83a7253c
shrutizalariya <146011537+shrutizalariya@users.noreply.github.com>
2025-05-11 13:15:25 +0530

    Add files via upload

---
+++ Python Programming/Lab - 11/calculator.py
@@ -0,0 +1,13 @@
+def Addition(a,b):
+    return a + b
+
+def Subtraction(a,b):
+    return a - b
+
+def Multiplication(a,b):
+    return a * b
+
+def Division(a,b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a/b
